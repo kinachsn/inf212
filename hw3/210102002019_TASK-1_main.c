8290ddd3f4adccb00ccbfabc6654a8d175dca9a9
kinachsn <hasankinacbusiness@gmail.com>
2023-03-24 10:15:34 +0300

    hw3 task1 is done

---
+++ hw3/210102002019_TASK-1_main.c
@@ -0,0 +1,29 @@
+/*
+ * main.c
+ *
+ *  Created on: 24 Mar 2023
+ *      Author: Hasan Kınaç
+ */
+#include <stdio.h>
+
+#define ARRAY_SIZE 		6
+
+int main(){
+	float array[ARRAY_SIZE];
+	float n;
+	float sum = 0;
+
+	for(int i = 0;i < ARRAY_SIZE;i++){
+		printf("Type an element of array : ");
+		scanf("%f",&n);
+		array[i] = n;
+		sum += n;
+	}
+	printf("elements of the array :\n");
+	for(int i = 0;i < ARRAY_SIZE;i++){
+		printf("%f\n",array[i]);
+	}
+
+	printf("the average of elements in float array : %f",sum / ARRAY_SIZE);
+}
+
